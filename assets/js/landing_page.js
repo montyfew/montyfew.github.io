db1512cff926b7322b02ae771c1490d5058601a7
Tom <thomas.hodson@ecmwf.int>
2024-03-27 12:58:29 +0000

    Add bio

--- assets/js/landing_page.js
+++ assets/js/landing_page.js
@@ -1,11 +1,13 @@
-const links = [...document.querySelectorAll('header a')];
+const links = [...document.querySelectorAll('header nav a')];
 const header = document.querySelector('header');
+const bio = document.querySelector('section.bio');
 
 links.map(a => {
   a.onclick = (e) => {
     const a = e.srcElement;
     e.preventDefault();
     header.classList.add('top');
+    bio.classList.add('top');
     setTimeout(function() { 
         window.location.href = a.href
     }, 1000)
@@ -15,4 +17,5 @@ links.map(a => {
 onpageshow = (event) => {
     console.log(event);
     header.classList.remove('top');
+    bio.classList.remove('top');
 };
--- index.md
+++ index.md
@@ -5,3 +5,6 @@ head: |
     <link rel="stylesheet" href="/assets/css/landing_page_css.css">
     <script src="/assets/js/landing_page.js" type = "module"></script>
 ---
+<section class = "bio">
+My audiovisual practice is informed by the relationship between music and place, having become intrigued by this theme after living in and undertaking musical projects in a handful of different cities across the UK. Through sonic explorations, film making and intermedial VJ techniques, I’m interested in exploring the intersections of music and the moving image.
+</section>
