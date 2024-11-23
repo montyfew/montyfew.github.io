fdc2269b3b4dfd6f7ff35c2677a197cd351d61ff
Tom <thomas.hodson@ecmwf.int>
2024-11-23 20:27:51 +0000

    disbale animation for phones

--- assets/js/all_pages.js
+++ assets/js/all_pages.js
@@ -1,25 +1,26 @@
 // Look for any hidden objects and smoothly make them visible by removing their hidden class
-const toReveal = [...document.querySelectorAll('.smooth-reveal')];
+const toReveal = [...document.querySelectorAll(".smooth-reveal")];
 
-toReveal.map(h => {
-    setTimeout(function() { 
-        h.classList.remove("hidden");
-    }, 10)
-})
+toReveal.map((h) => {
+  setTimeout(function () {
+    h.classList.remove("hidden");
+  }, 10);
+});
 
-const body = document.querySelector('body');
-const main = document.querySelector('main');
-const title = document.querySelector('header h1');
-const home = document.querySelector('header nav a.exit')
-const header = document.querySelector('header')
-home.onclick = (e) => {
+// The animation doesn't work so well on older phones so just
+// disable it for anything that seems like it might be a phone
+if (window.innerWidth > 620) {
+  const body = document.querySelector("body");
+  const home = document.querySelector("header nav a.exit");
+  home.onclick = (e) => {
     const a = e.srcElement;
     e.preventDefault();
-    if(window.location.pathname !== "/") {
-        body.classList.add('smooth-reveal');
-        body.classList.add('hidden');
-        setTimeout(function() { 
-            window.location.href = a.href
-        }, 1000)
+    if (window.location.pathname !== "/") {
+      body.classList.add("smooth-reveal");
+      body.classList.add("hidden");
+      setTimeout(function () {
+        window.location.href = a.href;
+      }, 1000);
     }
-};
+  };
+}
